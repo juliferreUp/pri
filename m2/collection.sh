638da6c801d10ae2d6ba3397579031628fd4ad7b
up201805327 <up201805327@fc.up.pt>
2022-11-11 17:33:34 +0000

    Improve indexing.json

--- m2/collection.sh
+++ m2/collection.sh
@@ -2,5 +2,4 @@
 
 curl -X POST -H 'Content-type:application/json' --data-binary @./indexing.json http://localhost:8983/solr/core/schema
 
-
 curl -X POST -H 'Content-type:application/csv' --data-binary @./data/wiki_movie_plots.csv http://localhost:8983/solr/core/update?commit=true
