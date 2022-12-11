c625220b1657fcf440ccda102d6bfd545dbdad40
up201805327 <up201805327@fc.up.pt>
2022-12-11 16:43:53 +0000

    Try suggester

--- m3/collection.sh
+++ m3/collection.sh
@@ -1,4 +1,5 @@
 # Populate collection
 
 curl -X POST -H 'Content-type:application/json' --data-binary @./indexing.json http://localhost:8983/solr/core/schema
+curl -X POST -H 'Content-type:application/json' --data-binary @./suggester.json http://localhost:8983/solr/     
 curl -X POST -H 'Content-type:application/csv' --data-binary @./data/wiki_movie_final.csv http://localhost:8983/solr/core/update?commit=true
